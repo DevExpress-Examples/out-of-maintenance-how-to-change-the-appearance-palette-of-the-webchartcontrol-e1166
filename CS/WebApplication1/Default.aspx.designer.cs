86f9219b54607da9c5a18cf641d9a4698d9f7192
DevExpressExampleBot <fakeEmail>
2022-12-07 16:44:37 +0400

    Source auto update [skip ci]

--- CS/WebApplication1/Default.aspx.designer.cs
+++ CS/WebApplication1/Default.aspx.designer.cs
@@ -44,6 +44,6 @@ namespace WebApplication1 {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::DevExpress.Web.ASPxEditors.ASPxComboBox ASPxComboBox1;
+        protected global::DevExpress.Web.ASPxComboBox ASPxComboBox1;
     }
 }
--- VB/WebApplication1/Default.aspx.designer.vb
+++ VB/WebApplication1/Default.aspx.designer.vb
@@ -47,6 +47,6 @@ Namespace WebApplication1
 		''' Auto-generated field.
 		''' To modify move field declaration from designer file to code-behind file.
 		''' </remarks>
-		Protected ASPxComboBox1 As Global.DevExpress.Web.ASPxEditors.ASPxComboBox
+		Protected ASPxComboBox1 As Global.DevExpress.Web.ASPxComboBox
 	End Class
 End Namespace
